7cd7223cc7f074ab95f466296e7199b3d0ee3c5d
SARVESH <>
2021-04-08 05:53:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        await message.channel.send(message.content[::-1])
+
+keep_alive()
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.run(token)
